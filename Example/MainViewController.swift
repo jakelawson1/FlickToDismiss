aeff726b10e8682329dfde1bef03c9141e58da6c
Jake Lawson <jakelawson1@hotmail.com>
2016-06-04 19:10:35 +0100

    Project setup
    Basic project setup and file structure.

---
+++ Example/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Jake Lawson on 04/06/2016.
+//  Copyright © 2016 Jake Lawson. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
